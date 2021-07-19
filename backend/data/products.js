e31eae94a075fb764bfc02190481869a9df540bf
leaf-turner <wesley.shaw.475@my.csun.edu>
2021-07-19 13:47:02 -0700

    fixed bug where quantity of product was getting to large

--- backend/data/products.js
+++ backend/data/products.js
@@ -4,7 +4,7 @@ const products = [{
       "https://www.nasa.gov/images/content/607690main_Kepler22bArtwork_800-600.jpg",
     description:
       "Class M , Terrestrial with beautiful warm oceans teaming with life.  74% nitrogen, 21 % oxygen, 4% helium, 1% trace gases",
-    price: 78999990,
+    price: 789990,
     countInStock: 1,
   },
   {
@@ -13,7 +13,7 @@ const products = [{
       "https://news-cdn.softpedia.com/images/news2/Gliese-581d-Confirmed-as-Habitable-2.jpg",
     description:
       "Class M, Terrestrial with large grassland type continents and large stretches of red sand beaches. 80% nitrogen, 19% oxygen, 1% trace gases.",
-    price: 10099999990,
+    price: 9000000,
     countInStock: 1,
   },
   {
@@ -21,17 +21,17 @@ const products = [{
     imageUrl:
       "https://images.squarespace-cdn.com/content/v1/54bf12d2e4b0f0d81bf74ee7/1602110514824-4PH6U0TXVMIEXGA0FFFP/image-asset.jpeg?format=750w",
     description:
-      "Asteroid belt, approximately 1.9 million iron based asteroids with possible GZ-123 deposits.",
-    price: 120000,
-    countInStock: 1900000,
+      "Asteroid belt, approximately 2000 iron based asteroids with possible GZ-123 deposits.",
+    price: 80,
+    countInStock: 2000,
   },
   {
     name: "Hazzcor A",
     imageUrl:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yMjg4Mzg1MC9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTY3MzkyNjMxM30",
+      "https://cdn.eso.org/images/thumb700x/eso1134a.jpg",
     description:
       "Class M, moon of gas giant with large Jungles and even larger creatures.",
-    price: 501500000,
+    price: 501500,
     countInStock: 1,
   },
 ];
